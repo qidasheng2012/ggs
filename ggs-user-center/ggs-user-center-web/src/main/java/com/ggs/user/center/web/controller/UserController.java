9265ba290d4650dd7c5018dc2b7a0ced0bf2aecb
qidasheng2012 <qidasheng2012@163.com>
2019-12-27 17:58:11 +0800

    edit

--- ggs-user-center/ggs-user-center-web/src/main/java/com/ggs/user/center/web/controller/UserController.java
+++ ggs-user-center/ggs-user-center-web/src/main/java/com/ggs/user/center/web/controller/UserController.java
@@ -2,13 +2,13 @@ package com.ggs.user.center.web.controller;
 
 
 @RestController
-@RequestMapping("/v1/orderUserApi")
+@RequestMapping("/v1/userApi")
 public class UserController {
 
     @Autowired
     private IUserService userService;
 
-    @PostMapping("list")
+    @PostMapping("/list")
     public ResultEntity list(@RequestBody UserKey key){
         PageInfo<User> pageInfo = userService.selectListByPage(CommonUtil.copyProperties(key, User.class), CommonUtil.getPageable(key));
         PageInfoVo<UserVo> pageInfoVo = CommonUtil.pageInfo2Vo(pageInfo, user -> CommonUtil.copyProperties(user, UserVo.class));
